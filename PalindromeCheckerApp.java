7e4393f0f3a4a8717c7cda56ea69aa0ab78a484b
Prasannadp46 <prasannadp46@gmail.com>
2026-03-18 01:18:37 +0530

    UC3 added

--- PalindromeCheckerApp.java
+++ PalindromeCheckerApp.java
@@ -1,17 +1,14 @@
 import java.util.Scanner;
-
-class PalindromeCheckerApp {
+public class PalindromeCheckerApp {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Input text: ");
         String input = sc.nextLine();
-        boolean isPalindrome = true;
-        for (int i = 0; i < input.length() / 2; i++) {
-            if (input.charAt(i) != input.charAt(input.length() - 1 - i)) {
-                isPalindrome = false;
-                break;
-            }
+        String reversed = "";
+        for (int i = input.length() - 1; i >= 0; i--) {
+            reversed = reversed + input.charAt(i);
         }
+        boolean isPalindrome = input.equals(reversed);
         System.out.println("Is it a palindrome? : " + isPalindrome);
     }
 }
