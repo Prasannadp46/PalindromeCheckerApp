afda8d5ade77898b6d2a91ebc100760a14aceb6c
Prasannadp46 <prasannadp46@gmail.com>
2026-03-18 01:38:30 +0530

    UC13 added

--- PalindromeCheckerApp.java
+++ PalindromeCheckerApp.java
@@ -7,25 +7,31 @@ public class PalindromeCheckerApp {
         System.out.print("Input: ");
         String input = sc.nextLine();
 
-        PalindromeStrategy strategy = new StackStrategy();
-        boolean result = strategy.check(input);
+        PalindromeStrategy stackStrategy = new StackStrategy();
+        PalindromeStrategy dequeStrategy = new DequeStrategy();
 
-        System.out.println("Is Palindrome? : " + result);
-        sc.close();
+        long start1 = System.nanoTime();
+        boolean res1 = stackStrategy.check(input);
+        long end1 = System.nanoTime();
+
+        long start2 = System.nanoTime();
+        boolean res2 = dequeStrategy.check(input);
+        long end2 = System.nanoTime();
+
+        System.out.println("Stack Result : " + res1);
+        System.out.println("Stack Time   : " + (end1 - start1) + " ns");
+
+        System.out.println("Deque Result : " + res2);
+        System.out.println("Deque Time   : " + (end2 - start2) + " ns");
     }
 }
 
-interface PalindromeStrategy {
-    boolean check(String input);
-}
+interface PalindromeStrategy {boolean check(String input);}
 
 class StackStrategy implements PalindromeStrategy {
-
     public boolean check(String input) {
         Stack<Character> stack = new Stack<>();
-
         for (char c : input.toCharArray()) stack.push(c);
-
         for (char c : input.toCharArray()) {
             if (c != stack.pop()) return false;
         }
@@ -34,12 +40,9 @@ class StackStrategy implements PalindromeStrategy {
 }
 
 class DequeStrategy implements PalindromeStrategy {
-
     public boolean check(String input) {
         Deque<Character> deque = new ArrayDeque<>();
-
         for (char c : input.toCharArray()) deque.addLast(c);
-
         while (deque.size() > 1) {
             if (deque.removeFirst() != deque.removeLast()) return false;
         }
